dd06b71ab090d996d85ec2b50d5bf9a5f3fdde19
Trisharati Pal <86723529+Trisharati@users.noreply.github.com>
2025-01-22 22:50:57 +0530

    Delete Schema.js

--- Schema.js
+++
@@ -1,9 +0,0 @@
-const mongoose=require('mongoose')
-const templateSchema = new mongoose.Schema({
-    
-    tem:{
-        type:String        
-    }   
-})
-
-module.exports = new mongoose.model('Templates',templateSchema,'Templates')
